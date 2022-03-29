964d4050bc6cd4a9e13db3645cc428831db36776
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2022-03-29 18:47:31 +0800

    update var

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send(`AWS deployment test NAME: ${process.env.DEV_NAME}`)
+    res.send(`AWS deployment test name: ${process.env.DEV_NAME}`)
 })
 
 app.listen(port, () => {
